b9ee9a849d966d42e5fa22528635fcbb17317f01
GloLiquid <glynne@gloliquid.com>
2014-06-15 12:23:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('enter').mouseenter(function(){
+        $('enter').fadeTo('fast',1);
+    });
+    $('enter').mouseleave(function(){
+        $('enter').fadeTo('fast',0.5);
+});
+});
