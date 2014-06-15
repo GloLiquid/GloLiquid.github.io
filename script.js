da2eab94e67d5a1621543a28b08cee64835a7fbf
GloLiquid <glynne@gloliquid.com>
2014-06-15 13:45:50 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#div button[id="enter"]').mouseenter(function(){
-        $('#div button[id="enter"]').fadeTo('fast',1);
-    });
-    $('#div button[id="enter"]').mouseleave(function(){
-        $('#div button[id="enter"]').fadeTo('fast',0.5);
-});
-});
