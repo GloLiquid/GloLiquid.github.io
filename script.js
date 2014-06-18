9f1dad741ae63a283cbbed7626d007ff4a9a4ae9
GloLiquid <glynne@gloliquid.com>
2014-06-18 16:52:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#startPause').mouseenter(function(){
+        $('#startPause').fadeTo('fast',0.5);
+    });
+    $('#startPause').mouseleave(function(){
+        $('#startPause').fadeTo('fast',1);
+});
+});
+
+$(document).ready(function(){
+    $('#reset').mouseenter(function(){
+        $('#reset').fadeTo('fast',0.5);
+    });
+    $('#reset').mouseleave(function(){
+        $('#reset').fadeTo('fast',1);
+});
+});
