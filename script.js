4eabd82cb139125b9d26c4c0981d00d6e573335f
GloLiquid <glynne@gloliquid.com>
2014-06-19 13:23:35 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#next').mouseenter(function(){
-        $('#next').fadeTo('fast',0.5);
-    });
-    $('#next').mouseleave(function(){
-        $('#next').fadeTo('fast',1);
-});
-});
