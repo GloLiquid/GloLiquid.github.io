4ff6e251fbd3122fec118039e703b4dad52aca98
GloLiquid <glynneowen@Phillips-iMac.local>
2014-07-12 17:13:41 +0100

    jquery
    draft 1 of box interactivity.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,57 @@
 //Problem: Text input instructions aren't explicit enough.
-
 //Solution: Add hints to focus user's thoughts and actions.
 
-//When box entered
-//Hint pop up
-//Find out if input is valid
 //Hide hint
+$("form span").hide();
+
+function firstEvent() {
+	//Find out if input is valid
+	if($(this).val().length > 8) {
+	//Hide hint if valid
+	$(this).next().hide();
+	} else {
+	//Else show hint
+	$(this).next().show();
+	}
+}
+
+//When event happens on why, how, what
+$("#first").focus(firstEvent).keyup(firstEvent);
+
+
+//Hide hint
+$("form span").hide();
+
+function secondEvent() {
+	//Find out if input is valid
+	if($(this).val().length > 8) {
+	//Hide hint if valid
+	$(this).next().hide();
+	} else {
+	//Else show hint
+	$(this).next().show();
+	}
+}
+
+//When event happens on why, how, what
+$("#second").focus(secondEvent).keyup(secondEvent);
+
+//Hide hint
+$("form span").hide();
+
+function thirdEvent() {
+	//Find out if input is valid
+	if($(this).val().length > 8) {
+	//Hide hint if valid
+	$(this).next().hide();
+	} else {
+	//Else show hint
+	$(this).next().show();
+	}
+}
+
+//When event happens on why, how, what
+$("#third").focus(thirdEvent).keyup(thirdEvent);
 
 
 //Problem: The information entered by the user is lost when the user finishes an objective.
