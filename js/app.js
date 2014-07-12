2085286c484fadcd9c8df1c75e3ae9389999bb47
GloLiquid <glynneowen@Phillips-iMac.local>
2014-07-12 14:53:56 +0100

    2:53

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
+//Problem: Text input instructions aren't explicit enough.
+
+//Solution: Add hints to focus user's thoughts and actions.
+
+//When box entered
+//Hint pop up
+//Find out if input is valid
+//Hide hint
+
+
 //Problem: The information entered by the user is lost when the user finishes an objective.
 	
 //Solution: Print out the information on the page.
 
-//1, When button pressed
-//2, Store information
-//3, Print out information
+//When button pressed
+//Store information
+//When button pressed
+//Print out information
