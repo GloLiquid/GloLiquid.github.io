50a7558e5ad16fdcd569997e03578e459800376d
GloLiquid <glynneowen@Phillips-iMac.local>
2014-07-12 12:58:52 +0100

    Html and css tweaks

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//Problem: Prevent users from being overwhelmed with information.
+
+//Hide task
+//Show task slowly
+$(".task").hide().show("slow");
+
+//Solution: Hide each subsequent step and reveal them through user interaction.
+
+//1, Hide step
+//2, Add a button
+//3, When button pressed
+	//3.1, Show next step
+	// 3.2, Get rid of button
